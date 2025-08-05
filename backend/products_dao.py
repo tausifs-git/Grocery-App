68625cb85038fbba2f87ff40f1b976a17e1671df
tausifs-git <tausifrahman2001@gmail.com>
2025-08-05 14:00:38 +0600

    Added insertion and deletion.

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -23,7 +23,37 @@ def get_all_products(connection):
     return response
 
 
+def insert_product (connection, product):
+    cursor = connection.cursor()
+    
+    query = ("INSERT INTO products (name, uom_id, price_per_unit) values (%s, %s, %s);")
+    # %s:- This are called parameterized values
+    
+    data = (product['product_name'], product['uom_id'], product['price_per_unit'])
+    
+    cursor.execute(query, data)
+    
+    connection.commit()
+    return cursor.lastrowid
+
+
+def delete_row (connection, row_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products WHERE product_id = " + str(row_id) + ";")
+    cursor.execute(query)
+    connection.commit()
+
+
 
 if __name__ == '__main__':
     connection = get_connection()
-    print(get_all_products(connection))
+    
+    # print(get_all_products(connection))
+    
+    # print(insert_product(connection, {
+    #     'product_name': 'carrot',
+    #     'uom_id': '1',
+    #     'price_per_unit': '40'
+    # }) )
+    
+    delete_row(connection, 3)
